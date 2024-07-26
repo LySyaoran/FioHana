5cf85f5aac4829d87f1bba911af1688115de2b3b
LySyaoran <ngao57257@gmail.com>
2024-07-26 12:58:52 +0700

    first commit

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Do_An.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Promotion()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ShopController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Do_An.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Shop()
+        {
+            return View();
+        }
+        public ActionResult ShopSingle()
+        {
+            return View();
+        }
+    }
+}
