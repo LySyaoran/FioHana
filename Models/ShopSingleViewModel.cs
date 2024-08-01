f588aa7e10a9ce70c7502b6495d3c1a7aaede063
phucquantang <phucquantang@gmail.com>
2024-08-01 22:26:35 +0700

    Thêm SingleShop view

---
+++ Models/ShopSingleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Do_An.Models
+{
+    public class ShopSingleViewModel
+    {
+        public product Product { get; set; }
+        public List<product> RelatedProducts { get; set; }
+        public string Occasion { get; set; }
+    }
+}
